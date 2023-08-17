292dde1b028ec8c173363f513f2f785f55ec0489
Julia Mazaia <140029817+Julia-Mazaia@users.noreply.github.com>
2023-08-17 16:30:42 -0300

    Updates

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 import validator from './validator.js';
+let realCardNumber =''
 
-console.log(validator);
 const cardNumberInput = document.getElementById('cardNumber');
 document.addEventListener('DOMContentLoaded', () => {
   const validateButton = document.getElementById('validateButton');
 
   validateButton.addEventListener('click', () => {
-    const cardNumber = cardNumberInput.value;
 
-    if (validator.isValid(cardNumber)) {
+    if (validator.isValid(realCardNumber)) {
       alert('Cartão válido!');
     } else {
       alert('Número de cartão inválido. Certifique-se de inserir um número válido.');
     }
+    realCardNumber = ''
   });
 });
 //Input para exibir apenas ultimos 4 números
-cardNumberInput.addEventListener('input', () => {
-  console.log('teste')
+cardNumberInput.addEventListener('input', (event) => {
+  realCardNumber += event.target.value.slice (-1)
   const maskedCardNumber = validator.maskify (cardNumberInput.value);
   cardNumberInput.value = maskedCardNumber;
 });
+
 
